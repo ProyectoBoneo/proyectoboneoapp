bb00f8b194ce2c5681aca131352a9d59f92de8c1
Alvaro Tuso <alvaro.tuso@adroll.com>
2018-08-12 20:04:40 -0300

    initial firebase setup

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import DeviceInfo from 'react-native-device-info';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import axios from 'axios';
@@ -7,7 +8,13 @@ import thunk from 'redux-thunk';
 import AuthenticationContainer from 'app/components/auth/AuthenticationContainer';
 import RootReducer from 'app/reducers/index';
 const api = axios.create();
-api.defaults.baseURL = "http://10.0.2.2:8000/api/";
+
+if (DeviceInfo.isEmulator()) {
+    api.defaults.baseURL = "http://10.0.2.2:8000/api/";
+} else {
+    api.defaults.baseURL = "http://192.168.0.33:8000/api/";   // TODO: test in device only with prod server?
+}
+
 api.defaults.headers.common['Content-Type'] = 'application/json';
 api.defaults.headers.common['Accept'] = 'application/json';
 
